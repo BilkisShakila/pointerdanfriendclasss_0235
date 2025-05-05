e9a61e32f88edbf84fa62ae54a2e7c0b5a422151
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-05 18:36:39 +0700

    add libarary

--- pointer.cpp
+++ pointer.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main()
-(
-    
-)
+{
+    //deference
+    int a =10;
+    int b =6;
+    int y =20;
+    int *c = &a;
+    cout << "Alamat Memori : " << c << endl;
+    cout << "Nilai dari c : " << *c << endl;
+    c = &y;
+    cout << "Alamat Memori : " << c << endl;
+    cout << "Nilai dari c : " << *c << endl;
+
+    //reference 
+    int 
+}
+
