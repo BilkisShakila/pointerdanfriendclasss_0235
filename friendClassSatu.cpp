502b13a360d173a62f0ed68fd3a56fe8e7113dbe
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-05 19:19:35 +0700

    add library

---
+++ friendClassSatu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class out_of_range
+{
+    private:
+    string nama;
+
+    public:
+    void setNama(string pNama);
+    friend class siswa; // friend class siswa
+};
