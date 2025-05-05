0cc2cec8eb438fa0c8911c149ff976c645296da4
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-05 19:01:14 +0700

    oopmahasiswacpp

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -3,12 +3,26 @@ using namespace std;
 
 class mahasiswa
 {
-    public :
+public :
     int nim;
-    void ShowNim;
-    {
-        cout << "No Induk = " << nim << endl;
-    }
+    void shownim(){
+        cout << "No Induk = "<<nim<<endl;
+    }    
 };
 
-int main()
+int main(){
+    mahasiswa mhs; // object mhs
+    mhs.nim = 2024; 
+    mhs.shownim(); //Member Acces operator
+
+    //Mengakses objek melalui reference (alias) — perubahan memengaruhi mhs.
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access operator
+    mhs.shownim();
+
+    //Mengakses objek melalui pointer menggunakan arrow operator (->).
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->shownim();
+    Return 0;
+};
